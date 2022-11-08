670b9278273cdff0f4c99db1a49fe98bec4d67ba
marius <zandermarius1@gmail.com>
2022-11-08 22:49:58 +0100

    update

---
+++ .config/nvim/lua/settings.lua
@@ -0,0 +1,16 @@
+local set = vim.opt
+
+set.ignorecase = true
+set.smartcase  = true
+set.wrap = false
+set.expandtab = true
+set.autoread = true
+set.rnu = true
+set.signcolumn = "no"
+set.colorcolumn = "100"
+set.history = 1000
+set.conceallevel = 0
+set.scrolloff = 3
+set.tabstop = 2
+set.shiftwidth = 2
+set.number = true
