73eff4092182891086399c271b8bf6002e12ab04
marius <zandermarius1@gmail.com>
2023-02-19 20:03:19 +0100

    update
    Signed-off-by: Marius Zander <hello@mzndr.me>

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,6 +12,7 @@ abbr -a ll  "exa -la --git --group-directories-first"
 abbr -a cat "bat"
 abbr -a q   "exit"
 abbr -a dcup "docker compose up -d && docker compose logs -f"
+abbr -a ts 'tmux new -s $(basename $(pwd))'
 
 fish_vi_key_bindings
 
