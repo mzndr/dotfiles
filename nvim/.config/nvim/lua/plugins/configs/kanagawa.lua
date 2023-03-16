c72b643ada473058dcad9016c859cdd873d7e328
marius <marius@bam-bam-bam.com>
2023-03-16 09:25:07 +0100

    update
    Signed-off-by: Marius Zander <hello@mzndr.me>

--- nvim/.config/nvim/lua/plugins/configs/kanagawa.lua
+++ nvim/.config/nvim/lua/plugins/configs/kanagawa.lua
@@ -13,8 +13,6 @@ require('kanagawa').setup({
     dimInactive = false,        -- dim inactive window `:h hl-NormalNC`
     globalStatus = false,       -- adjust window separators highlight for laststatus=3
     terminalColors = true,      -- define vim.g.terminal_color_{0,17}
-    colors = {},
-    overrides = function(colord) return {} end,
     theme = "default"           -- Load "default" theme or the experimental "light" theme
 })
 
