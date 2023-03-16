1f6be7e3a96999c01f7a789341d1af862f01eaa7
marius <marius@bam-bam-bam.com>
2023-03-16 08:30:47 +0100

    update
    Signed-off-by: Marius Zander <hello@mzndr.me>

---
+++ fish/.config/fish/conf.d/vi_mode.fish
@@ -0,0 +1,6 @@
+# See: https://fishshell.com/docs/current/interactive.html#vi-mode-commands
+
+set fish_cursor_default block
+set fish_cursor_insert line
+set fish_cursor_replace_one underscore
+set fish_cursor_visual block
