cb63da3f863ecef9db004549e90b3c2ceb87a777
marius <zandermarius1@gmail.com>
2023-03-30 09:59:39 +0200

    update
    Signed-off-by: Marius Zander <hello@mzndr.me>

--- nvim/.config/nvim/lua/plugins/configs/kanagawa.lua
+++ nvim/.config/nvim/lua/plugins/configs/kanagawa.lua
@@ -1,19 +1,27 @@
 -- Default options:
 require('kanagawa').setup({
-    undercurl = true,           -- enable undercurls
-    commentStyle = { italic = true },
-    functionStyle = {},
-    keywordStyle = { italic = true},
-    statementStyle = { bold = true },
-    typeStyle = {},
-    variablebuiltinStyle = { italic = true},
-    specialReturn = true,       -- special highlight for the return keyword
-    specialException = true,    -- special highlight for exception handling keywords
-    transparent = true,        -- do not set background color
-    dimInactive = false,        -- dim inactive window `:h hl-NormalNC`
-    globalStatus = false,       -- adjust window separators highlight for laststatus=3
-    terminalColors = true,      -- define vim.g.terminal_color_{0,17}
-    theme = "default"           -- Load "default" theme or the experimental "light" theme
+  undercurl = true,           -- enable undercurls
+  commentStyle = { italic = true },
+  functionStyle = {},
+  keywordStyle = { italic = true},
+  statementStyle = { bold = true },
+  typeStyle = {},
+  variablebuiltinStyle = { italic = true},
+  specialReturn = true,       -- special highlight for the return keyword
+  specialException = true,    -- special highlight for exception handling keywords
+  transparent = true,         -- do not set background color
+  dimInactive = false,        -- dim inactive window `:h hl-NormalNC`
+  globalStatus = false,       -- adjust window separators highlight for laststatus=3
+  terminalColors = true,      -- define vim.g.terminal_color_{0,17}
+  theme = "default",          -- Load "default" theme or the experimental "light" theme
+  overrides = function(colors)
+    return {
+      FloatBorder = { bg = 'NONE' },
+      NormalFloat = { bg = 'NONE' },
+      LineNr = { bg = 'NONE'},
+      SignColumn = { bg = 'NONE'},
+    }
+  end,
 })
 
 -- setup must be called before loading
