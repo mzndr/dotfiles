3724a61e6cc4f098f6539e3ad1df393f9429a0d5
marius <zandermarius1@gmail.com>
2023-03-31 09:21:50 +0200

    update
    Signed-off-by: Marius Zander <hello@mzndr.me>

--- nvim/.config/nvim/lua/plugins/configs/kanagawa.lua
+++ nvim/.config/nvim/lua/plugins/configs/kanagawa.lua
@@ -15,11 +15,28 @@ require('kanagawa').setup({
   terminalColors = true,      -- define vim.g.terminal_color_{0,17}
   theme = "default",          -- Load "default" theme or the experimental "light" theme
   overrides = function(colors)
+    local theme = colors.theme
+    local innerColor = 'NONE';
+    local borderBg = 'NONE';
+    local borderFg = theme.ui.bg_p2;
     return {
       FloatBorder = { bg = 'NONE' },
       NormalFloat = { bg = 'NONE' },
       LineNr = { bg = 'NONE'},
       SignColumn = { bg = 'NONE'},
+      TelescopeTitle = { fg = theme.ui.special, bold = true },
+      TelescopePromptBorder = { fg = borderFg, bg = borderBg },
+      TelescopeResultsBorder = { fg = borderFg, bg = borderBg },
+      TelescopePreviewBorder = { bg = borderBg, fg = borderFg },
+
+      TelescopeResultsNormal = { bg = innerColor },
+      TelescopePromptNormal = { bg = innerColor },
+      TelescopePreviewNormal = { bg = innerColor },
+
+      Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+      PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+      PmenuSbar = { bg = theme.ui.bg_m1 },
+      PmenuThumb = { bg = theme.ui.bg_p2 },
     }
   end,
 })
