1de7a0e9d3564da4c03e771e104ea7f77ee299b1
marius <zandermarius1@gmail.com>
2023-04-15 01:19:27 +0200

    update

--- nvim/.config/nvim/lua/plugins/configs/kanagawa.lua
+++ nvim/.config/nvim/lua/plugins/configs/kanagawa.lua
@@ -33,10 +33,13 @@ require('kanagawa').setup({
       TelescopePromptNormal = { bg = innerColor },
       TelescopePreviewNormal = { bg = innerColor },
 
+      CocFloating = { bg =  "#1F1F28" },
+
       Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
       PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
       PmenuSbar = { bg = theme.ui.bg_m1 },
       PmenuThumb = { bg = theme.ui.bg_p2 },
+      
     }
   end,
 })
