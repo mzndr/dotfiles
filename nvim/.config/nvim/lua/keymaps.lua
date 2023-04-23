e212a24a85928901a8951178aeaf5173662e5ba7
marius <zandermarius1@gmail.com>
2023-04-23 16:28:37 +0200

    update
    Signed-off-by: Marius Zander <hello@mzndr.me>

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -2,3 +2,8 @@ vim.g.mapleader = " "
 vim.keymap.set('i', 'jj', '<Esc>',{})
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
+
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+Y')
+
