f34da2bf12fc24ec7505d83c898545432626885e
marius <zandermarius1@gmail.com>
2023-04-23 16:13:42 +0200

    update
    Signed-off-by: Marius Zander <hello@mzndr.me>

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,7 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set('i', 'jj', '<Esc>',{})
-
-local options = { noremap=true, silent=true }
-
-vim.api.nvim_set_keymap('', '<S-Up>', '<nop>', options)
-vim.api.nvim_set_keymap('', '<S-Down>', '<nop>', options)
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
