aa13b4b61f45f3491e2ee707103fd5da767b7d44
marius <marius@bam-bam-bam.com>
2023-05-09 17:02:12 +0200

    update

--- fish/.config/fish/abbrs.fish
+++ fish/.config/fish/abbrs.fish
@@ -9,4 +9,4 @@ abbr -a ts 'tmux new -s $(basename $(pwd))'
 abbr -a ta 'tmux attach'
 abbr -a cw 'cargo watch - '
 abbr -a v 'nvim'
-abbr -a tree  "exa -la --git --group-directories-first --tree"
+abbr -a tree  "exa -la --git --git-ignore --group-directories-first --tree"
