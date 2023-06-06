36548c9385571e3b206fda61aad137d5b5eb2acf
marius <zandermarius1@gmail.com>
2023-06-06 12:27:27 +0200

    update

--- awesome/.config/awesome/themes/my-theme/theme.lua
+++ awesome/.config/awesome/themes/my-theme/theme.lua
@@ -7,7 +7,7 @@ local xresources = require("beautiful.xresources")
 local dpi = xresources.apply_dpi
 local gfs = require("gears.filesystem")
 local themes_path = "~/.config/awesome/themes/" 
-local bg_image = "~/wallpapers/waneella/purple_blossom_street.jpg";
+local bg_image = "~/wallpapers/keyboard_switch_sushi.png";
 
 local theme = {}
 
--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -2,7 +2,7 @@ local set = vim.keymap.set
 
 vim.g.mapleader = " "
 
-set("i", "jj", "<Esc>")
+-- set("i", "jj", "<Esc>")
 set("n", "n", "nzzzv")
 set("n", "N", "Nzzzv")
 
@@ -14,8 +14,8 @@ set("n", "<leader>y", '"+y')
 set("n", "<leader>Y", '"+Y')
 set("v", "<leader>y", '"+y')
 
-set("n", "<C-j>", "<Plug>(coc-diagnostic-prev)", { silent = true })
-set("n", "<C-k>", "<Plug>(coc-diagnostic-next)", { silent = true })
+set("n", "<C-j>", "<Plug>(coc-diagnostic-next)", { silent = true })
+set("n", "<C-k>", "<Plug>(coc-diagnostic-prev)", { silent = true })
 
 set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
