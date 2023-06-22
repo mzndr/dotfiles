93017ee30c0632165d74d9f0bbbd60609b53d2fe
Marius Zander <marius@bam-bam-bam.com>
2023-06-23 01:54:13 +0200

    update
    Signed-off-by: Marius Zander <hello@mzndr.me>

---
+++ fish/.config/fish/functions/checklist.fish
@@ -0,0 +1,44 @@
+# ~/.config/fish/functions/checklist.fish
+function checklist
+    set -l bins \
+        bat \
+        brew \
+        btm \
+        curl \
+        docker \
+        exa \
+        fish \
+        fnm \
+        git \
+        gpg \
+        gsed \
+        lazygit \
+        mkcert \
+        nvim \
+        oha \
+        ouch \
+        paru \
+        ranger \
+        rg \
+        rustup \
+        scp \
+        ssh \
+        starship \
+        stow \
+        tmux
+
+    set -l installed_count 0
+
+    for bin in (string pad $bins)
+        echo -sn $bin " "
+
+        if fish -c "command -q $bin"
+            set installed_count (math $installed_count + 1)
+            echo -s (set_color green)  (set_color normal)
+        else
+            echo -s (set_color red)  (set_color normal)
+        end
+    end
+
+    echo -s \n $installed_count / (count $bins) " installed"
+end
