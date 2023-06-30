bfa514bed2b29d7703da03ff7e60b705d58982cb
marius <marius@bam-bam-bam.com>
2023-06-30 11:49:50 +0200

    update
    Signed-off-by: Marius Zander <hello@mzndr.me>

--- awesome/.config/awesome/themes/my-theme/theme.lua
+++ awesome/.config/awesome/themes/my-theme/theme.lua
@@ -24,8 +24,8 @@ theme.fg_focus      = "#ffffff"
 theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
-theme.useless_gap   = dpi(0)
-theme.border_width  = dpi(0)
+theme.useless_gap   = dpi(2)
+theme.border_width  = dpi(2)
 theme.border_normal = "#1F1F28"
 theme.border_focus = "#2F2F28"
 theme.border_marked = "#91231c"
