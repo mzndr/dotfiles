e113ef369b588dd30c23451104a5904dc9bf587a
marius <marius@bam-bam-bam.com>
2023-07-18 13:07:22 +0200

    update

--- nvim/.config/nvim/lua/plugins/configs/telescope.lua
+++ nvim/.config/nvim/lua/plugins/configs/telescope.lua
@@ -6,4 +6,4 @@ telescope.load_extension("coc")
 
 vim.keymap.set("n", "<leader>f", builtin.find_files, {})
 vim.keymap.set("n", "<leader>gg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>d", builtin.diagnostics, {})
+vim.keymap.set("n", "<leader>d", telescope.extensions.coc.diagnostics, {})
