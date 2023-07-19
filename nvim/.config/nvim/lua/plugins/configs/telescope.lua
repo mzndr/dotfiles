a51218277b899661da7c8822af6f1744a296b3b6
Marius Zander <marius@bam-bam-bam.com>
2023-07-20 00:17:41 +0200

    update

--- nvim/.config/nvim/lua/plugins/configs/telescope.lua
+++ nvim/.config/nvim/lua/plugins/configs/telescope.lua
@@ -2,7 +2,7 @@ local builtin = require("telescope.builtin")
 local telescope = require("telescope")
 
 telescope.load_extension("harpoon")
-telescope.load_extension("coc")
+-- telescope.load_extension("coc")
 
 vim.keymap.set("n", "<leader>f", builtin.find_files, {})
 vim.keymap.set("n", "<leader>gg", builtin.live_grep, {})
