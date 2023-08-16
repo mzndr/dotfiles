d4ced2d085b1d3539e9fa3410180374e2bd47f58
Marius Zander <marius@bam-bam-bam.com>
2023-08-16 13:09:43 +0200

    update

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,96 +1,104 @@
 local is_installed, packer = pcall(require, "packer")
 local function install_packer()
-	vim.fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim",
-	})
-	vim.cmd("packadd packer.nvim")
-	packer = require("packer")
+  vim.fn.system({
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim",
+  })
+  vim.cmd("packadd packer.nvim")
+  packer = require("packer")
 end
 
 if not is_installed then
-	install_packer()
+  install_packer()
 end
 
 packer.startup(function(use)
-	use({ "wbthomason/packer.nvim" })
+  use({ "wbthomason/packer.nvim" })
 
-	use({
-		"machakann/vim-highlightedyank",
-		config = function()
-			require("plugins/configs/highlightedyank")
-		end,
-	})
+  use({
+    "machakann/vim-highlightedyank",
+    config = function()
+      require("plugins/configs/highlightedyank")
+    end,
+  })
 
-	use({
-		"tpope/vim-fugitive",
-		config = function()
-			require("plugins/configs/fugitive")
-		end,
-	})
+  use({
+    "tpope/vim-fugitive",
+    config = function()
+      require("plugins/configs/fugitive")
+    end,
+  })
 
-	use({
-		"tpope/vim-surround",
-		config = function()
-			require("plugins/configs/surround")
-		end,
-	})
+  use({
+    "tpope/vim-surround",
+    config = function()
+      require("plugins/configs/surround")
+    end,
+  })
 
-	use({
-		"rebelot/kanagawa.nvim",
-		config = function()
-			require("plugins/configs/kanagawa")
-		end,
-	})
+  use({
+    "rebelot/kanagawa.nvim",
+    config = function()
+      require("plugins/configs/kanagawa")
+    end,
+  })
 
-	use({
-		"ThePrimeagen/harpoon",
-		config = function()
-			require("plugins/configs/charpoon")
-		end,
-	})
+  use({
+    "ThePrimeagen/harpoon",
+    config = function()
+      require("plugins/configs/charpoon")
+    end,
+  })
 
-	use({
-		"907th/vim-auto-save",
-		config = function()
-			require("plugins/configs/autosave")
-		end,
-	})
+  use({
+    "907th/vim-auto-save",
+    config = function()
+      require("plugins/configs/autosave")
+    end,
+  })
 
-	use({
-		"neoclide/coc.nvim",
-		branch = "release",
-		config = function()
-			require("plugins/configs/coc")
-		end,
-	})
+  use({
+    "neoclide/coc.nvim",
+    branch = "release",
+    config = function()
+      require("plugins/configs/coc")
+    end,
+  })
 
-	use({
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.0",
-		requires = { { "nvim-lua/plenary.nvim" } },
-		after = { "nvim-treesitter" },
-		config = function()
-			require("plugins/configs/telescope")
-		end,
-	})
+  use({
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.0",
+    requires = { { "nvim-lua/plenary.nvim" } },
+    after = { "nvim-treesitter" },
+    config = function()
+      require("plugins/configs/telescope")
+    end,
+  })
 
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		config = function()
-			require("plugins/configs/treesitter")
-		end,
-	})
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("plugins/configs/treesitter")
+    end,
+  })
 
-	use({
-		"nvim-treesitter/nvim-treesitter-context",
-		after = { "nvim-treesitter" },
-		config = function()
-			require("plugins/configs/treesitter-context")
-		end,
-	})
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true },
+    config = function()
+      require("plugins/configs/clualine")
+    end,
+  }
+
+  use({
+    "nvim-treesitter/nvim-treesitter-context",
+    after = { "nvim-treesitter" },
+    config = function()
+      require("plugins/configs/treesitter-context")
+    end,
+  })
 end)
