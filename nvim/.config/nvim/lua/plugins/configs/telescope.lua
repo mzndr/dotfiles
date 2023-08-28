0607c39990cba6758aae5b5bcd9be5ad7cfb6c83
Marius Zander <marius@bam-bam-bam.com>
2023-08-28 12:18:53 +0200

    update
    Signed-off-by: Marius Zander <hello@mzndr.me>

--- nvim/.config/nvim/lua/plugins/configs/telescope.lua
+++ nvim/.config/nvim/lua/plugins/configs/telescope.lua
@@ -5,3 +5,8 @@ telescope.load_extension("harpoon")
 
 vim.keymap.set("n", "<leader>f",  builtin.find_files, {})
 vim.keymap.set("n", "<leader>gg", builtin.live_grep, {})
+vim.keymap.set("n", "<Leader>gs", builtin.lsp_document_symbols)
+vim.keymap.set("n", "<Leader>gd", builtin.lsp_definitions)
+vim.keymap.set("n", "<Leader>gt", builtin.lsp_type_definitions)
+vim.keymap.set("n", "<Leader>gi", builtin.lsp_implementations)
+vim.keymap.set("n", "<Leader>gr", builtin.lsp_references)
