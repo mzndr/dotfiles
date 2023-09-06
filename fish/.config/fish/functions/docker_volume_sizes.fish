d191b30b2960d333aad7a87036763d3ebd0cfcef
Marius Zander <marius@bam-bam-bam.com>
2023-09-06 09:40:39 +0200

    update
    Signed-off-by: Marius Zander <hello@mzndr.me>

--- fish/.config/fish/functions/docker_volume_sizes.fish
+++ fish/.config/fish/functions/docker_volume_sizes.fish
@@ -1,9 +1,10 @@
 #!/usr/bin/fish
 
-echo -e "Volume\t\t\t\t\t\t\t\t\tSize" # Very professional
 docker volume ls -q |
 while read vol;
   set mount (docker volume inspect "$vol" | jq -r '.[0].Mountpoint')
   set size (sudo du -sh "$mount" | sed 's/\s.*$//')
-  echo -e "$vol\t$size"
+  set table (string join '' "$table\n$vol  $size")
 end
+
+echo -e $table | column -t -N "Volume,Size"
