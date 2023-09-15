47b9330724bde07e9d16405a9e359b93b701d3a9
Marius Zander <marius@bam-bam-bam.com>
2023-09-15 09:24:00 +0200

    update

--- fish/.config/fish/functions/uuid.fish
+++ fish/.config/fish/functions/uuid.fish
@@ -1,34 +1,36 @@
 #!/usr/bin/fish
 
-set _flag_n 1
+function uuid
+  set _flag_n 1
 
-argparse -i -x s,S 'n#number' 's/string' 'S/singlequote' 'c/csv' 'a/array' -- $argv
+  argparse -i -x s,S 'n#number' 's/string' 'S/singlequote' 'c/csv' 'a/array' -- $argv
 
-if set -q _flag_a
-  echo [
-end
-
-for i in (seq 1 "$_flag_n")
-  set uid (/bin/cat /proc/sys/kernel/random/uuid)
-  set print $uid
-  
-  if set -q _flag_s
-    set print \"$uid\"
-  else if set -q _flag_S
-    set print \'$uid\'
+  if set -q _flag_a
+    echo [
   end
 
-  if set -q _flag_c; or set -q _flag_a
-    set print $print,
+  for i in (seq 1 "$_flag_n")
+    set uid (/bin/cat /proc/sys/kernel/random/uuid)
+    set print $uid
+    
+    if set -q _flag_s
+      set print \"$uid\"
+    else if set -q _flag_S
+      set print \'$uid\'
+    end
+
+    if set -q _flag_c; or set -q _flag_a
+      set print $print,
+    end
+
+    if set -q _flag_a
+      set print \t$print
+    end
+
+    echo $print
   end
 
   if set -q _flag_a
-    set print \t$print
+    echo ]
   end
-
-  echo $print
-end
-
-if set -q _flag_a
-  echo ]
 end
