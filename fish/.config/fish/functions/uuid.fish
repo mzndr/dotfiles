8538e2f4509ff4375081699f8e6a255395c62226
Marius Zander <marius@bam-bam-bam.com>
2023-09-15 09:15:31 +0200

    update
    Signed-off-by: Marius Zander <hello@mzndr.me>

--- fish/.config/fish/functions/uuid.fish
+++ fish/.config/fish/functions/uuid.fish
@@ -1,1 +1,33 @@
-/bin/cat /proc/sys/kernel/random/uuid
+#!/usr/bin/fish
+
+argparse -i 'n#number' 's/string' 'S/singlequote' 'c/csv' 'a/array' -- $argv
+
+if set -q _flag_a
+  echo [
+end
+
+for i in (seq 1 "$_flag_n")
+  set uid (/bin/cat /proc/sys/kernel/random/uuid)
+  set print $uid
+  
+  if set -q _flag_s
+    set print \"$uid\"
+  else if set -q _flag_S
+    set print \'$uid\'
+  end
+
+  if set -q _flag_c; or set -q _flag_a
+    set print $print,
+  end
+
+  if set -q _flag_a
+    set print \t$print
+  end
+
+
+  echo $print
+end
+
+if set -q _flag_a
+  echo ]
+end
