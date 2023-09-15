5ee36528119d8839984b8fce2d4602c6dd9931d4
Marius Zander <marius@bam-bam-bam.com>
2023-09-15 09:28:35 +0200

    update
    Signed-off-by: Marius Zander <hello@mzndr.me>

--- fish/.config/fish/functions/uuid.fish
+++ fish/.config/fish/functions/uuid.fish
@@ -4,6 +4,7 @@ function uuid --description 'Generate UUIDs'
   set _flag_n 1
 
   argparse -i -x s,S 'n#number' 's/string' 'S/singlequote' 'c/csv' 'a/array' -- $argv
+  or return
 
   if set -q _flag_a
     echo [
