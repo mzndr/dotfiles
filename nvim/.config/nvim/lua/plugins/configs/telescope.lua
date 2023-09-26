c8c715fe3f6440727b32c90442eec7e21bae4ac4
Marius Zander <marius@bam-bam-bam.com>
2023-09-26 16:19:26 +0200

    update
    Signed-off-by: Marius Zander <hello@mzndr.me>

--- nvim/.config/nvim/lua/plugins/configs/telescope.lua
+++ nvim/.config/nvim/lua/plugins/configs/telescope.lua
@@ -2,6 +2,11 @@ local builtin = require("telescope.builtin")
 local telescope = require("telescope")
 
 telescope.setup({
+  pickers = {
+    find_files = {
+      hidden = true
+    },
+  },
   extensions = {
     coc = {
         prefer_locations = true, -- always use Telescope locations to preview definitions/declarations/implementations etc
