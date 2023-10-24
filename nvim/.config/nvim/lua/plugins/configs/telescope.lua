5a3e1912c259d3933b9e5cc1f6f7cc8da04f12ba
Marius Zander <marius@bam-bam-bam.com>
2023-10-24 19:16:35 +0200

    update

--- nvim/.config/nvim/lua/plugins/configs/telescope.lua
+++ nvim/.config/nvim/lua/plugins/configs/telescope.lua
@@ -17,8 +17,10 @@ vim.keymap.set("n", "<leader>gg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>gj", builtin.diagnostics, {})
 vim.keymap.set("n", "<leader>gq",  builtin.quickfix, {})
 
-vim.keymap.set("n", "<Leader>gs", builtin.lsp_document_symbols)
 vim.keymap.set("n", "<Leader>gd", builtin.lsp_definitions)
 vim.keymap.set("n", "<Leader>gr", builtin.lsp_references)
 vim.keymap.set("n", "<Leader>gt", builtin.lsp_type_definitions)
 vim.keymap.set("n", "<Leader>gi", builtin.lsp_implementations)
+
+vim.keymap.set("n", "<Leader>gl", builtin.git_commits)
+vim.keymap.set("n", "<Leader>gs", builtin.git_status)
