ed8773921131ecd9057ae2610a8ad5202a5ccd47
Marius Zander <marius@bam-bam-bam.com>
2023-12-08 10:25:29 +0100

    update
    Signed-off-by: Marius Zander <hello@mzndr.me>

--- fish/.config/fish/conf.d/envs.fish
+++ fish/.config/fish/conf.d/envs.fish
@@ -1,5 +1,6 @@
 set -x GLUSR mzndr
 set -x GLPAT (cat "$HOME/glpat.token")
+set -x OPENAI_KEY (cat "$HOME/openai-api.key")
 set -x PATH "$HOME/.cargo/bin:$PATH"
 set -x PATH "$HOME/scripts:$PATH"
 set -x PATH "$(yarn global bin):$PATH"
--- polybar/launch_polybar.sh
+++ polybar/launch_polybar.sh
@@ -2,7 +2,7 @@
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload &
   done
 else 
   polybar --reload example
