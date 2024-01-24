ff170b4d87cddac1588b4c0edc4322c128bcabd1
Marius Zander <marius@bam-bam-bam.com>
2024-01-24 09:29:37 +0100

    update

--- awesome/.config/awesome/themes/my-theme/theme.lua
+++ awesome/.config/awesome/themes/my-theme/theme.lua
@@ -7,7 +7,7 @@ local xresources = require("beautiful.xresources")
 local dpi = xresources.apply_dpi
 local gfs = require("gears.filesystem")
 local themes_path = "~/.config/awesome/themes/" 
-local bg_image = "~/wallpapers/waneella/city_night.png";
+local bg_image = "~/wallpapers/waneella/cold_street.png";
 
 local theme = {}
 
