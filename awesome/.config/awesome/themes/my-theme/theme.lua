d39f6a4353c7cad3cf3270fdaf1d41978a3e9ce2
Marius Zander <marius@bam-bam-bam.com>
2024-05-07 16:15:23 +0200

    update

--- awesome/.config/awesome/themes/my-theme/theme.lua
+++ awesome/.config/awesome/themes/my-theme/theme.lua
@@ -7,8 +7,7 @@ local xresources = require("beautiful.xresources")
 local dpi = xresources.apply_dpi
 local gfs = require("gears.filesystem")
 local themes_path = "~/.config/awesome/themes/";
--- local bg_image = "~/wallpapers/waneella/office_desk.jpg";
-local bg_image = "~/wallpapers/space.jpg";
+local bg_image = "~/wallpapers/waneella/orange_cyberpunk_rooftops.jpg";
 
 local theme = {}
 
