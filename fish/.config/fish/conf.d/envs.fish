4dddeaf118a5ab1012b187515ca9e56585e1d20a
Marius Zander <hello@mzndr.me>
2024-10-14 10:48:14 +0200

    update

--- fish/.config/fish/conf.d/envs.fish
+++ fish/.config/fish/conf.d/envs.fish
@@ -2,6 +2,5 @@ set -x GLUSR mzndr
 set -x GLPAT (cat "$HOME/glpat.token")
 set -x PATH "$HOME/.cargo/bin:$PATH"
 set -x PATH "$HOME/scripts:$PATH"
-set -x PATH "$(yarn global bin):$PATH"
 set -x EDITOR "nvim"
 
