be8a9378c19d8470fa26ce07a0652a18cd020645
Marius Zander <hello@mzndr.me>
2024-10-21 09:46:26 +0200

    update

--- fish/.config/fish/conf.d/envs.fish
+++ fish/.config/fish/conf.d/envs.fish
@@ -1,5 +1,4 @@
 set -x GLUSR mzndr
-set -x GLPAT (cat "$HOME/glpat.token")
 set -x PATH "$HOME/.cargo/bin:$PATH"
 set -x PATH "$HOME/scripts:$PATH"
 set -x EDITOR "nvim"
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,3 +5,6 @@ end
 set -x LC_ALL en_US.UTF-8
 set -x LC_CTYPE en_US.UTF-8
 fish_vi_key_bindings
+
+direnv hook fish | source
+
