ec78d838982a4ac3f9325aa3fd60552a364f596d
Marius Zander <hello@mzndr.me>
2024-11-12 13:46:13 +0100

    update

--- fish/.config/fish/conf.d/envs.fish
+++ fish/.config/fish/conf.d/envs.fish
@@ -1,5 +1,13 @@
+set -x LC_ALL en_US.UTF-8
+set -x LC_CTYPE en_US.UTF-8
+
 set -x GLUSR mzndr
 set -x PATH "$HOME/.cargo/bin:$PATH"
-set -x PATH "$HOME/scripts:$PATH"
 set -x EDITOR "nvim"
+
+set -x GOPROXY="https://goproxy.your-cloud.host|https://riverqueue.com/goproxy|https://proxy.golang.org|direct"
+set -x GONOSUMDB="git.hetzner.company,riverqueue.com/riverpro"
+set -x GOPRIVATE="git.hetzner.company"
+
+set -x GLPAT (cat ~/glpat)
 
