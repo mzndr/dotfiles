d72a994b8422749841538a5d455e62c743e240ee
Marius Zander <hello@mzndr.me>
2024-12-08 22:50:26 +0100

    update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,6 +6,8 @@ fish_add_path ~/go/bin
 
 fish_vi_key_bindings
 
-direnv hook fish | source
+if type -q direnv
+  direnv hook fish | source
+end
 
 set -q KREW_ROOT; and set -gx PATH $PATH $KREW_ROOT/.krew/bin; or set -gx PATH $PATH $HOME/.krew/bin
