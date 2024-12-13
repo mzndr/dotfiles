281a43f6f6ea52519e74d0e44ef7942427e09079
Marius Zander <hello@mzndr.me>
2024-12-13 11:57:18 +0100

    update

--- nvim/.config/nvim/lua/plugins/configs/telescope.lua
+++ nvim/.config/nvim/lua/plugins/configs/telescope.lua
@@ -2,51 +2,51 @@ local builtin = require("telescope.builtin")
 local telescope = require("telescope")
 
 telescope.setup({
-	pickers = {
-		find_files = {
-			find_command = {
-				"rg",
-				"--files",
-				"--hidden",
-				"--glob",
-				"!*/.git/*",
-				"--glob",
-				"!*/.sqlx/*",
-				"--glob",
-				"!.sqlx/*",
-				"--glob",
-				"!.git/*",
-				"--glob",
-				"!*.lock",
-				"--glob",
-				"!*/package-lock.json",
-				"--glob",
-				"!*/pnpm-lock.yaml",
-				"--glob",
-				"!*.png",
-				"--glob",
-				"!*.jpeg",
-				"--glob",
-				"!*.jpg",
-			},
-		},
-		live_grep = {
-			additional_args = function()
-				return { "--hidden" }
-			end,
-			glob_pattern = {
-				"!*/.git/*",
-				"!.git/*",
-				"!*/.sqlx/*",
-				"!*.lock",
-				"!*.png",
-				"!*.jpg",
-				"!*.jpeg",
-				"!*/package-lock.json",
-				"!*/pnpm-lock.yaml",
-			},
-		},
-	},
+  pickers = {
+    find_files = {
+      find_command = {
+        "rg",
+        "--files",
+        "--hidden",
+        "--glob",
+        "!*/.git/*",
+        "--glob",
+        "!*/.sqlx/*",
+        "--glob",
+        "!.sqlx/*",
+        "--glob",
+        "!.git/*",
+        "--glob",
+        "!*.lock",
+        "--glob",
+        "!*/package-lock.json",
+        "--glob",
+        "!*/pnpm-lock.yaml",
+        "--glob",
+        "!*.png",
+        "--glob",
+        "!*.jpeg",
+        "--glob",
+        "!*.jpg",
+      },
+    },
+    live_grep = {
+      additional_args = function()
+        return { "--hidden" }
+      end,
+      glob_pattern = {
+        "!*/.git/*",
+        "!.git/*",
+        "!*/.sqlx/*",
+        "!*.lock",
+        "!*.png",
+        "!*.jpg",
+        "!*.jpeg",
+        "!*/package-lock.json",
+        "!*/pnpm-lock.yaml",
+      },
+    },
+  },
 })
 
 telescope.load_extension("harpoon")
@@ -60,6 +60,7 @@ vim.keymap.set("n", "<Leader>gd", builtin.lsp_definitions)
 vim.keymap.set("n", "<Leader>gr", builtin.lsp_references)
 vim.keymap.set("n", "<Leader>gt", builtin.lsp_type_definitions)
 vim.keymap.set("n", "<Leader>gi", builtin.lsp_implementations)
+vim.keymap.set("n", "<Leader>gs", builtin.lsp_document_symbols)
+vim.keymap.set("n", "<Leader>gS", builtin.lsp_dynamic_workspace_symbols)
 
 vim.keymap.set("n", "<Leader>gl", builtin.git_commits)
-vim.keymap.set("n", "<Leader>gs", builtin.git_status)
