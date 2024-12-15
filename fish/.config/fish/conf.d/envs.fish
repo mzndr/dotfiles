6f018bdc365626a1a3b19c8919bf7df962a39b16
Marius Zander <hello@mzndr.me>
2024-12-15 15:21:56 +0100

    update

--- fish/.config/fish/conf.d/envs.fish
+++ fish/.config/fish/conf.d/envs.fish
@@ -9,5 +9,9 @@ set -x GOPROXY "https://goproxy.your-cloud.host|https://riverqueue.com/goproxy|h
 set -x GONOSUMDB "git.hetzner.company,riverqueue.com/riverpro"
 set -x GOPRIVATE "git.hetzner.company"
 
-set -x GLPAT (cat ~/tokens/glpat.token)
-set -x HC_API_STAGING_TOKEN (cat ~/tokens/staging_loadbalancer_auth.token)
+if test -e ~/tokens/glpat.token
+  set -x GLPAT (cat ~/tokens/glpat.token)
+end
+if test -e ~/tokens/staging_loadbalancer_auth.token
+  set -x HC_API_STAGING_TOKEN (cat ~/tokens/staging_loadbalancer_auth.token)
+end
