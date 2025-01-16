cba3a4bfc3d2614bd8d044f4d8b76a3674d6f573
Marius Zander <hello@mzndr.me>
2025-01-16 07:21:40 +0100

    update

--- fish/.config/fish/conf.d/abbrs.fish
+++ fish/.config/fish/conf.d/abbrs.fish
@@ -1,39 +1,35 @@
+abbr -a c 'clear'
+abbr -a cat "bat"
+abbr -a cw 'cargo watch - '
+abbr -a dcps 'docker compose ps --format "table {{.ID}}\t{{.Name}}\t{{.Ports}}\t{{.RunningFor}}\t{{.Status}}"'
+abbr -a dcup "docker compose up -d && docker compose logs -f"
+abbr -a dec-to-hex 'printf "0x%x\n" '
+abbr -a dps 'docker ps --format "table {{.ID}}\t{{.Names}}\t{{.Ports}}\t{{.RunningFor}}\t{{.Status}}"'
+abbr -a duf 'duf -hide loops'
+abbr -a fcd 'cd (dirname (fzf))'
 abbr -a fish-edit-config 'cd ~/dotfiles/fish/.config/fish && nvim ./config.fish'
 abbr -a fish-reload-config 'source ~/.config/fish/**/*.fish'
-
-abbr -a dec-to-hex 'printf "0x%x\n" '
-
-abbr -a ll  "exa -la --git --group-directories-first"
-abbr -a cat "bat"
-abbr -a q   "exit"
-abbr -a dcup "docker compose up -d && docker compose logs -f"
-
-abbr -a ts 'TMUX_SESSION_DIR=$(pwd) tmux new -s $(basename $TMUX_SESSION_DIR)'
-abbr -a ta 'tmux attach'
-
-abbr -a cw 'cargo watch - '
-abbr -a v 'nvim'
-abbr -a t "exa -a --git --git-ignore --group-directories-first --tree"
-abbr -a dps 'docker ps --format "table {{.ID}}\t{{.Names}}\t{{.Ports}}\t{{.RunningFor}}\t{{.Status}}"'
-abbr -a dcps 'docker compose ps --format "table {{.ID}}\t{{.Name}}\t{{.Ports}}\t{{.RunningFor}}\t{{.Status}}"'
 abbr -a fn 'find . -name '
-abbr -a fzf 'fzf --preview="cat {}"'
 abbr -a fvim 'nvim (\'fzf --preview="cat {}"\')'
-abbr -a fcd 'cd (dirname (fzf))'
-
-abbr -a gs 'git status --short '
-abbr -a gd 'git diff'
-abbr -a gp 'git push'
-abbr -a gc 'git commit'
-abbr -a gca 'git commit --amend'
-abbr -a gl 'git log --oneline'
-abbr -a glh 'git log --oneline -n 10'
-abbr -a glg 'git log --oneline --graph'
-
-abbr -a c 'clear'
-abbr -a duf 'duf -hide loops'
-
+abbr -a fzf 'fzf --preview="cat {}"'
 abbr -a icat 'kitten icat'
-
 abbr -a k 'kubectl'
 abbr -a kc 'kubectl config use-context'
+abbr -a ll  "exa -la --git --group-directories-first"
+abbr -a q   "exit"
+abbr -a t "exa -a --git --git-ignore --group-directories-first --tree"
+abbr -a ta 'tmux attach'
+abbr -a ts 'TMUX_SESSION_DIR=$(pwd) tmux new -s $(basename $TMUX_SESSION_DIR)'
+abbr -a v 'nvim'
+
+# Git
+abbr -a gc  'git commit'
+abbr -a gca 'git commit --amend'
+abbr -a gd  'git diff'
+abbr -a gl  'git log --oneline'
+abbr -a glg 'git log --oneline --graph'
+abbr -a glh 'git log --oneline -n 10'
+abbr -a gp  'git push'
+abbr -a gr  'git rebase'
+abbr -a gri 'git rebase -i'
+abbr -a gs  'git status --short '
