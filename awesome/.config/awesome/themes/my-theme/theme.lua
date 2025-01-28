14e05c65ed9667aaff30f6a3f70aa98921ca9598
Marius Zander <hello@mzndr.me>
2025-01-28 08:31:13 +0100

    update

--- awesome/.config/awesome/themes/my-theme/theme.lua
+++ awesome/.config/awesome/themes/my-theme/theme.lua
@@ -26,8 +26,8 @@ theme.fg_focus = "#ffffff"
 theme.fg_urgent = "#ffffff"
 theme.fg_minimize = "#ffffff"
 
-theme.useless_gap = dpi(3)
-theme.border_width = dpi(4)
+theme.useless_gap = dpi(0)
+theme.border_width = dpi(2)
 theme.border_normal = "#303050"
 theme.border_focus = "#5F5F88"
 theme.border_marked = "#91231c"
